4cd189fdf825318019cfdaca4a61817c4ae35956
Marnus van Wyk <marnus.vanwyk@iotnxt.com>
2024-09-28 18:43:42 +0200

    push to dev

--- src/build_all.sh
+++ src/build_all.sh
@@ -3,4 +3,4 @@ export $(grep -v '^#' .env | xargs -d '\n')
 export $(grep -v '^#' ../helm/.env | xargs -d '\n')
 eval "$(sops -d --input-type dotenv --output-type dotenv .env.secrets | grep -v '^#' | sed 's/^/export /')"
 docker buildx bake --push --builder=container
-docker buildx prune --all
+docker buildx prune --all --force
