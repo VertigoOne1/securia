42ff8988bd2fbdb03f8341c3079828ad2583b6be
Marnus van Wyk <marnus.vanwyk@iotnxt.com>
2024-09-29 09:49:10 +0200

    move charts

--- helm/push_all.sh
+++ helm/push_all.sh
@@ -11,11 +11,11 @@ helm package charts/securia_api --version ${CHART_VERSION}
 helm package charts/securia_ui --version ${CHART_VERSION}
 helm package charts/yolo_processor --version ${CHART_VERSION}
 
-helm push collector-hikvision-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
-helm push collector-simulator-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
-helm push image-preprocessor-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
-helm push securia-api-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
-helm push securia-ui-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
-helm push yolo-processor-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia
+helm push collector-hikvision-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
+helm push collector-simulator-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
+helm push image-preprocessor-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
+helm push securia-api-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
+helm push securia-ui-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
+helm push yolo-processor-${CHART_VERSION}.tgz oci://harbor.marnus.com:443/securia/charts
 
 rm *tgz
